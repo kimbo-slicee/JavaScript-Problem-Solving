042169883840d4e7ff0022be90f8a6ff51d93268
mohammed el aouri <mohammedelaouri@gmail.com>
2024-11-17 02:12:35 +0100

    New Problem : Sum all numbers of a given array expect the highest and the lowest element

--- beginners-Level/Sum_Without_The_Highest_And_The_Lowest_Number.js
+++ beginners-Level/Sum_Without_The_Highest_And_The_Lowest_Number.js
@@ -1,4 +1,4 @@
-/**
+/*
  * - Task:
  * Sum all numbers of a given array expect the highest and the lowest element (by value not by index)
  * The highest or Lowest respectively a single element at each edge even if there are more than one withe same value
@@ -6,28 +6,32 @@
  * - Example:
  * {6,2,1,8,10}=16
  * {1,1,11,2,3}=6
- * */
+ * 
+ * /
 
 /**
- * Solution
- *
+ * Solution 1;
+ * check if empty Value returen 0
+ * fnid the heighest and lowest Number 
+ * fillter the array 
+ * summall numbers in the array 
  */
-function sum_Without_The_Highest_And_The_Lowest_Number(array){
-    let highest=0;
-    let lowest=0
-    // let index =0;
-    for(let i=0; i<array.length ; i++ ){
-      if(array[i]>array[i+1]){
-          highest=array[i];
-      }else if(array[i]<array[i+1]){
-          highest=array[i+1];
+function Highest_And_The_Lowest_Number(array){
+    if(array!==null){
+        let max=Math.max(...array);
+        let min=Math.min(...array);
+        indexofMaxNumber=array.indexOf(max);
+        indexofMinNumber=array.indexOf(min);
+        return array.filter((ele,index)=>index!==indexofMaxNumber && index!==indexofMinNumber).reduce((prev,curr)=>prev+curr);
 
-      }
-    }
-    console.log(highest)
+        }
 }
-sum_Without_The_Highest_And_The_Lowest_Number( [6,2,1,8,10,23,15,100,1,2,3]);
+console.log(Highest_And_The_Lowest_Number([1,1,11,2,3]));
+
 /**
- * second Solution Using ESC 6
- *
- * */
+ * Solution 2;
+ * sort the array and sum the numbers from the third to the second last element
+ */
+const solve_by_sorting=(array)=>array.sort((a,b)=>a-b).slice(1, array.length - 1).reduce((prev, curr) => prev + curr)
+    
+console.log(solve_by_sorting([1,1,11,2,3]))
