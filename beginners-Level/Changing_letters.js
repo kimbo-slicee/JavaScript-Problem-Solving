c4d005dfd04979198b353ee54b2f59f74a4681f8
mohammed el aouri <mohammedelaouri@gmail.com>
2024-11-18 02:00:19 +0100

    using regex pattern to resolve Changing_letters chalange '

--- beginners-Level/Changing_letters.js
+++ beginners-Level/Changing_letters.js
@@ -7,46 +7,41 @@
  * Vowels are: a, e, i, o, u 
  * alphabet: b, c, d, f, g, h, j, k, l, m, n, p, q, r, s, t, v, w, x, y and z .
  */
-
+//[1]
+ let vowels = ['a', 'e', 'i', 'o', 'u'];
  const  swap=(string)=> {
-    if(){
-
-    }
-    let vowels = ['a', 'e', 'i', 'o', 'u'];
-    return string.split('').map(ele=>{
-        if( vowels.includes(ele.toLowerCase())){
-            ele.toUpperCase();
-        }
-    }).join('');
-    
+   if(/@/.test(string) || /^$/.test(string)){
+	return string
+   }else if(/[aeiou]/i.test(string)){
+	return  string.split('').map(ele=>vowels.includes(ele)?ele.toUpperCase():ele).join('');
+   }
   }
- swap("Hello World!");
-
+console.log( swap("ABRACADABRA"));
 
 /**
  * Sample Tests 
  * 
  */
 
- const strictEqual = require('chai').assert.strictEqual;
-
-function doTest (input, expected) {
-	const actual = swap(input);
-	strictEqual(actual, expected, `for string:\n"${input}"\n`);
-}
-
-describe("Tests suite", () => {
-	it("sample tests", () => {
-		doTest("", "");
-		doTest("   @@@", "   @@@");
-		doTest("HelloWorld!", "HEllOWOrld!");
-		doTest("Sunday", "SUndAy");
-		doTest("Codewars", "COdEwArs");
-		doTest("Monday", "MOndAy");
-		doTest("Friday", "FrIdAy");
-		doTest("abracadabra", "AbrAcAdAbrA");
-		doTest("AbrAcAdAbrA", "AbrAcAdAbrA");
-		doTest("ABRACADABRA", "ABRACADABRA");
-		doTest("aBRaCaDaBRa", "ABRACADABRA");
-	});
-});
+//  const strictEqual = require('chai').assert.strictEqual;
+//
+// function doTest (input, expected) {
+// 	const actual = swap(input);
+// 	strictEqual(actual, expected, `for string:\n"${input}"\n`);
+// }
+//
+// describe("Tests suite", () => {
+// 	it("sample tests", () => {
+// 		doTest("", "");
+// 		doTest("   @@@", "   @@@");
+// 		doTest("HelloWorld!", "HEllOWOrld!");
+// 		doTest("Sunday", "SUndAy");
+// 		doTest("Codewars", "COdEwArs");
+// 		doTest("Monday", "MOndAy");
+// 		doTest("Friday", "FrIdAy");
+// 		doTest("abracadabra", "AbrAcAdAbrA");
+// 		doTest("AbrAcAdAbrA", "AbrAcAdAbrA");
+// 		doTest("ABRACADABRA", "ABRACADABRA");
+// 		doTest("aBRaCaDaBRa", "ABRACADABRA");
+// 	});
+// });
